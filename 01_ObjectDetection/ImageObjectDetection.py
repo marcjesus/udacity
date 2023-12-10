2537f2a29e0424bed6149d5ba877901ff4fc1fdc
Marc Jesus <marcjesusgarcia@gmail.com>
2023-12-10 15:12:38 +0000

    Open CV detection update

--- 01_ObjectDetection/ImageObjectDetection.py
+++ 01_ObjectDetection/ImageObjectDetection.py
@@ -1,30 +1,36 @@
 import cv2
+import os
+import imageio
 
 # Load the pre-trained classifiers for car and person detection
 car_classifier = cv2.CascadeClassifier('models/haarcascade_car.xml')
 person_classifier = cv2.CascadeClassifier('models/haarcascade_fullbody.xml')
 
-# Read the image
-image = cv2.imread('test_video/frame_71.png')
+# Add images in array
+images_folder = 'test_video'
+image_files = [os.path.join(images_folder, file) for file in os.listdir(images_folder) if file.endswith(('png', 'jpg', 'jpeg'))]
 
-# Convert the image to grayscale
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+images = []
+for image in sorted(image_files):
+    # Convert the image to grayscale
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    # Detect cars in the image
+    cars = car_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    # Detect persons in the image
+    persons = person_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-# Detect cars in the image
-cars = car_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    # Draw rectangles around the detected cars
+    for (x, y, w, h) in cars:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-# Detect persons in the image
-persons = person_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    # Draw rectangles around the detected persons
+    for (x, y, w, h) in persons:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-# Draw rectangles around the detected cars
-for (x, y, w, h) in cars:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    # Resize the image (to keep the same aspect ratio, use interpolation=cv2.INTER_AREA)
+    img_resized = cv2.resize(img, (200, 200), interpolation=cv2.INTER_AREA)
+    images.append(img_resized)
 
-# Draw rectangles around the detected persons
-for (x, y, w, h) in persons:
-    cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
-# Display the image with detections
-cv2.imshow('Object Detection', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+imageio.mimsave('OPENCV_output_gif.gif', images, duration=0.5)  # Adjust duration between frames as needed
--- 01_ObjectDetection/README.md
+++ 01_ObjectDetection/README.md
@@ -1,7 +1,10 @@
 # COMPUTER VISION
+Original video
+![](https://github.com/marcjesus/udacity/blob/main/01_ObjectDetection/output.gif)
 
 ## Using OPENCV
-
+Detecting cars and people using the opencv and basic training model
+![](https://github.com/marcjesus/udacity/blob/main/01_ObjectDetection/OPENCV_output.gif)
 
 ## Training a model
 
